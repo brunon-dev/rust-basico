c6d40da9ac32d7b08f0136723ac474a4adbe1e42
Bruno Figueiredo Nogueira <bruno@brunon.com.br>
2022-01-11 23:48:34 -0300

    Faz ajuste no código

--- hello.rs
+++ hello.rs
@@ -1,4 +1,6 @@
 fn main() {
+    // RAII - Resource acquisition is initialization
+    // tipagem estática e forte, mas em alguns contextos o compilador faz a inferencia de tipo da variável
     let name = "Usuário";
     println!("Alô mundo, {}!", name); // Macro
 }
